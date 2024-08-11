654f70ae22e1c4bb8fdb50cb5a0217115f5b0927
toaster08 <y10star.hon@gmail.com>
2024-08-11 21:17:19 +0900

    first commit

---
+++ DailyPractice/ContentView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  DailyPractice
+//
+//  Created by 山田　天星 on 2024/08/11.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+struct Contact: Identifiable {
+    let id: UUID
+    let name: String
+    
+    init(id: UUID = UUID(), name: String) {
+        self.id = id
+        self.name = name
+    }
+}
+
+class ContactsViewModel: ObservableObject {
+    @Published var contacts: [Contact] = []
+    @Published var searchText: String = ""
+    
+    var searchResults: [Contact] {
+        if searchText.isEmpty {
+            return contacts
+        } else {
+            return contacts.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+
+    init() {
+        contacts = [
+            Contact(name: "John Doe"),
+            Contact(name: "Jane Smith"),
+            Contact(name: "Alice Johnson"),
+            Contact(name: "Bob Brown")
+        ]
+    }
+}
+
+struct ContactsView: View {
+    let contact: Contact
+    
+    var body: some View {
+        VStack {
+            Text(contact.name)
+                .font(.largeTitle)
+                .padding()
+            Spacer()
+        }
+        .navigationTitle("Contact Detail")
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject private var viewModel = ContactsViewModel()
+
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(viewModel.searchResults) { contact in
+                    NavigationLink {
+                        ContactsView(contact: contact)
+                    } label: {
+                        Text(contact.name)
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .navigationTitle("Contacts")
+            .searchable(text: $viewModel.searchText)
+            .overlay {
+                if viewModel.searchResults.isEmpty {
+                    ContentUnavailableView.search
+                }
+            }
+        }
+   }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyPractice/DailyPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyPracticeApp.swift
+//  DailyPractice
+//
+//  Created by 山田　天星 on 2024/08/11.
+//
+
+import SwiftUI
+
+@main
+struct DailyPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
